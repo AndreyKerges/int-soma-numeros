533be1177b9db391f26f16e2d873e3de82619f2c
AndreyKerges <andrey.nascimento4@etc.sp.gov.br>
2023-03-24 21:26:09 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int n1, n2;
+
+Console.Write("Digite um número: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite outro número: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+int soma;
+
+soma = n1 + n2;
+
+Console.WriteLine($"A soma entre {n1} e {n2} é {soma}");
